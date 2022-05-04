ae4db671c3d45cdcb4ed2d8eaade26be3e952ab3
Dastorin <kfp.toxa@gmail.com>
2022-05-05 03:26:43 +1200

    update step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,10 @@
-const { program } = require('commander');
+#!/usr/bin/env node
+
+import { program } from 'commander';
 
 program
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.');
-
-program.parse();
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format, <type>', 'output format')
+  .parse();
