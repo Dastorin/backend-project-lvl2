3ed3e7ec66d4c40c6dec6a98806d5a7f28b7e2ff
Dastorin <kfp.toxa@gmail.com>
2022-07-09 17:34:29 +1200

    update index.js #step 6

--- src/index.js
+++ src/index.js
@@ -1,48 +1,40 @@
-#!/usr/bin/env node
-
 import _ from 'lodash';
 import parser from './parses.js';
 
 const isKey = (obj, key) => _.keys(obj).includes(key);
+const isTwoObjects = (value1, value2) => _.isObject(value1) && _.isObject(value2);
 
-const formater = (key, replacer = '    ', value = '') => `${replacer}${key}: ${value}`
+const formater = (date) => {
+  return console.log((date))
+};
 
 const genDiff = (filepath1, filepath2) => {
   const file1 = parser(filepath1);
   const file2 = parser(filepath2);
-
-  const iter = (obj1, obj2, deep) => {
+  const iter = (obj1, obj2) => {
 
     const result = _.uniq([..._.keys(obj1), ..._.keys(obj2)])
       .sort()
       .map((key) => {
-          if (_.isObject(obj1[key]) && _.isObject(obj2[key])){
-            return `${formater(key)}: ${iter(obj1[key], obj2[key], deep += 1)}\n`;
-          }
-          if (_.isObject(obj1[key])){
-            return `${formater(key, '  - ')}: ${iter(obj1[key], {}, deep += 1)}\n`
-          }
-          if (_.isObject(obj2[key])) {
-            return `${formater(key, '  + ')}: ${iter({}, obj2[key], deep += 1)}\n`
-          }
           if (isKey(obj1, key) && isKey(obj2, key)) {
+            if (isTwoObjects(obj1[key], obj2[key])) {
+              return {key: key, type: '2', value: iter(obj1[key], obj2[key])};
+            }
+          
             return obj1[key] === obj2[key]
-              ? `${formater(key, '    ', obj1[key])}\n`
-              : `${formater(key, '  - ', obj1[key])}\n${formater(key, '  + ', obj2[key])}\n`
+              ? {key: key, type: '=', value: obj1[key]}
+              : {key:key, type: '!=', value: [obj1[key], obj2[key]]};
           }
           if (isKey(obj1, key)){
-            return `${formater(key, '  - ', obj1[key])}\n`
+            return {key: key, type: 'first', value: obj1[key]};
           }
           if (isKey(obj2, key)) {
-            return `${formater(key, '  + ', obj2[key])}\n`
+            return {key: key, type: 'second', value: obj2[key]};
           }
       });
-    return (`{\n${result.join('')}}`);
+    return formater(result);
   };
-
-
-
-  return iter(file1, file2, 1);
+  return iter(file1, file2);
 };
 
 export default genDiff;
