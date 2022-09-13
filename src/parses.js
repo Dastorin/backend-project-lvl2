809913a4344e3d6c8434737fd03bd9753b7e8266
Dastorin <kfp.toxa@gmail.com>
2022-09-14 02:42:28 +0900

    update parser.js

--- src/parses.js
+++ src/parses.js
@@ -5,12 +5,12 @@ import path from 'path';
 export default (filepath) => {
   const format = path.extname(filepath);
   const data = fs.readFileSync(filepath);
-  switch (format) {
-    case '.json':
+  switch (format.slice(1)) {
+    case 'json':
       return JSON.parse(data);
-    case '.yml':
+    case 'yml':
       return yaml.load(data);
-    case '.yaml':
+    case 'yaml':
       return yaml.load(data);
     default:
       return console.error(new Error(`unknown format ${data}!`));
