f66a0ac22a709a0cb405930fc9f868965a54a6a5
Dastorin <kfp.toxa@gmail.com>
2022-09-19 21:50:53 +1200

    note 3 update index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import parser from './parsers.js';
 import getComparison from './comparison.js';
 import formater from './formaters/index.js';
 
+const getTypeData = (filepath) => path.extname(filepath).slice(1);
+const readFile = (filepath) => fs.readFileSync(filepath);
+
 export default (filepath1, filepath2, type) => {
-  const typeData1 = path.extname(filepath1).slice(1);
-  const data1 = fs.readFileSync(filepath1);
-
-  const typeData2 = path.extname(filepath2).slice(1);
-  const data2 = fs.readFileSync(filepath2);
-
-  const file1 = parser(data1, typeData1);
-  const file2 = parser(data2, typeData2);
+  const file1 = parser(readFile(filepath1), getTypeData(filepath1));
+  const file2 = parser(readFile(filepath2), getTypeData(filepath2));
   return formater(getComparison(file1, file2), type);
 };
